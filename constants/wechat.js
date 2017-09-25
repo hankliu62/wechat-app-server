32c06c0d96eb3d49468adc85b3ce266386523f3b
‘hankliu’ <397694072@qq.com>
2017-09-26 00:13:05 +0800

    feat(wechat): refine wechat controller

---
+++ constants/wechat.js
@@ -0,0 +1,21 @@
+export const FEMALE = 'female';
+export const MALE = 'male';
+
+// 聊天室类型
+// 群聊
+export const CHAT_ROOM_TYPE_GROUP = 'group';
+// 一对一单聊
+export const CHAT_ROOM_TYPE_FRIENDS = 'friends';
+// 公众号推送
+export const CHAT_ROOM_TYPE_SERVICE = 'service';
+
+
+// 新的朋友 添加状态
+// 等待确认
+export const CONSTANT_ADD_STATUS_AWAIT = 0;
+// 接受
+export const CONSTANT_ADD_STATUS_ACCEPT = 1;
+// 过期
+export const CONSTANT_ADD_STATUS_EXPIRE = 2;
+// 拒绝
+export const CONSTANT_ADD_STATUS_REFUSE = 3;
--- controllers/wechat.js
+++ controllers/wechat.js
@@ -1,6 +1,10 @@
 const Router = require('koa-router');
 const request = require('request');
 const pinyin = require('pinyin');
+const sortBy = require('lodash/sortBy');
+const cloneDeep = require('lodash/cloneDeep');
+
+const Constant = require('../constants/wechat');
 
 const router = new Router({prefix: '/v1/api/wechat'});
 
@@ -14,7 +18,7 @@ const getItemValue = (item, keys) => {
   return '';
 }
 
-const generatePinyinInitial = (items, keys) => {
+const generatePinyinInitial = (items, keys, supplement) => {
   const pinyinOptions = {
     heteronym: false,
     style: pinyin.STYLE_NORMAL // 设置拼音风格
@@ -33,29 +37,85 @@ const generatePinyinInitial = (items, keys) => {
       }
       item.pinyin = ([]).concat.call([], ...pinyins).join('');
     }
+
+    if (supplement && typeof(supplement) === 'function') {
+      supplement(item)
+    }
   }
 
   return items;
 }
 
 const getChats = () => {
-  const chats = require('../constants/data-chats.json') || [];
+  const chats = require('../data/data-chats.json') || [];
   return chats;
 }
 
 router.get('/chats', (ctx) => {
   const chats = getChats();
+  const groups = getGroups();
+  const contacts = getContacts();
+  const officialAccounts = getOfficialAccounts();
+
+  for (const chat of chats) {
+    let baseResources = [];
+    let chatMemberResources = [];
+    switch (chat.base.type) {
+      case Constant.CHAT_ROOM_TYPE_FRIENDS: {
+        baseResources = contacts;
+        chatMemberResources = contacts;
+        break;
+      }
+      case Constant.CHAT_ROOM_TYPE_GROUP: {
+        baseResources = groups;
+        chatMemberResources = contacts;
+        break;
+      }
+      case Constant.CHAT_ROOM_TYPE_SERVICE: {
+        baseResources = officialAccounts;
+        chatMemberResources = officialAccounts;
+        break;
+      }
+    }
+
+    const base = baseResources.find(item => item.wxid === chat.base.wxid) || {};
+    chat.base = Object.assign({}, cloneDeep(base), cloneDeep(chat.base));
+
+    if (chat.base.type === Constant.CHAT_ROOM_TYPE_FRIENDS) {
+      chat.base.name = chat.base.remark || chat.base.nickname;
+    }
+
+    if (chat.base.type === Constant.CHAT_ROOM_TYPE_SERVICE) {
+      const chatDialogues = (chat.chatDialogueModel || []).map((wxid) => {
+        const officialAccount = chatMemberResources.find(item => item.wxid === wxid) || {};
+        return Object.assign({ wxid }, cloneDeep(officialAccount));
+      });
+      chat.chatDialogueModel = chatDialogues;
+    } else {
+      const chatMembers = (chat.chatMemberModel || []).map((wxid) => {
+        const member = chatMemberResources.find(item => item.wxid === wxid) || {};
+        return Object.assign({ wxid }, cloneDeep(member));
+      });
+      chat.chatMemberModel = chatMembers;
+    }
+
+  }
+
+  const sortedChats = sortBy(chats, (item) => -item.chatBaseModel.endTimeStr);
+
   ctx.body = {
     status: 200,
     data: {
-      items: chats,
+      items: sortedChats
     }
   };
 });
 
 const getContacts = () => {
-  const contacts = require('../constants/data-contacts.json');
-  return generatePinyinInitial(contacts, ['remark', 'nickname']);
+  const contacts = require('../data/data-contacts.json');
+  return generatePinyinInitial(contacts, ['remark', 'nickname'], (item) => {
+    item.gender = item.sex === 1 ? Constant.MALE : Constant.FEMALE;
+  });
 }
 
 router.get('/contacts', (ctx) => {
@@ -63,7 +123,7 @@ router.get('/contacts', (ctx) => {
   ctx.body = {
     status: 200,
     data: {
-      items: contacts,
+      items: contacts
     }
   };
 });
@@ -79,7 +139,7 @@ router.get('/contacts/:wxid', (ctx) => {
 });
 
 const getGroups = () => {
-  const groups = require('../constants/data-groups.json');
+  const groups = require('../data/data-groups.json');
   return groups;
 }
 
@@ -88,13 +148,13 @@ router.get('/groups', (ctx) => {
   ctx.body = {
     status: 200,
     data: {
-      items: groups,
+      items: groups
     }
   };
 });
 
 const getMoments = () => {
-  const moments = require('../constants/data-moments.json');
+  const moments = require('../data/data-moments.json');
   return moments;
 }
 
@@ -103,13 +163,24 @@ router.get('/moments', (ctx) => {
   ctx.body = {
     status: 200,
     data: {
-      items: moments,
+      items: moments
+    }
+  };
+});
+
+router.get('/moments/:wxid', (ctx) => {
+  const moments = getMoments() || [];
+  const moment = moments.find(item => item.wxid === ctx.params.wxid);
+  ctx.body = {
+    status: 200,
+    data: {
+      moment
     }
   };
 });
 
 const getNewFriends = () => {
-  const newFriends = require('../constants/data-new-friends.json');
+  const newFriends = require('../data/data-new-friends.json');
   return newFriends;
 }
 
@@ -118,14 +189,14 @@ router.get('/new-friends', (ctx) => {
   ctx.body = {
     status: 200,
     data: {
-      items: newFriends,
+      items: newFriends
     }
   };
 });
 
 const getOfficialAccounts = () => {
-  const officialAccounts = require('../constants/data-official-accounts.json');
-  return officialAccounts;
+  const officialAccounts = require('../data/data-official-accounts.json');
+  return generatePinyinInitial(officialAccounts, ['name']);
 }
 
 router.get('/official-accounts', (ctx) => {
@@ -133,13 +204,14 @@ router.get('/official-accounts', (ctx) => {
   ctx.body = {
     status: 200,
     data: {
-      items: officialAccounts,
+      items: officialAccounts
     }
   };
 });
 
 const getSelf = () => {
-  const personal = require('../constants/data-self.json');
+  const personal = require('../data/data-self.json');
+  personal.gender = personal.sex === 1 ? Constant.MALE : Constant.FEMALE;
   return personal;
 }
 
@@ -148,7 +220,7 @@ router.get('/self', (ctx) => {
   ctx.body = {
     status: 200,
     data: {
-      item: personal,
+      item: personal
     }
   };
 });
